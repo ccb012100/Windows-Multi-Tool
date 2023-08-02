a9c8378190b31460467c55dc240782bb25ef8861
Christopher Bocardo <397636+ccb012100@users.noreply.github.com>
2023-08-02 17:26:59 -0500

    feat: initial commit
    This builds a running WPF application that runs in the system
    notifications tray and has a window with a single button that can be
    clicked to exit the program.

---
+++ WindowsMultiTool/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Drawing;
+using System.Windows;
+
+namespace WindowsMultiTool
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            Console.WriteLine("entered MainWindow");
+            // InitializeComponent call is required to merge the UI
+            // that is defined in markup with this class, including  
+            // setting properties and registering event handlers
+            InitializeComponent();
+
+            var notifyIcon = new System.Windows.Forms.NotifyIcon
+            {
+                BalloonTipText = "Windows Multi-tool has started",
+                Icon = new Icon(@".\Icons\multitool.ico"),
+                Text = "Windows Multi-Tool",
+                Visible = true
+            };
+
+            notifyIcon.ShowBalloonTip(3000);
+        }
+
+        void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
---
+++ WindowsMultiTool/ObjectExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json;
+
+namespace WindowsMultiTool
+{
+    internal static class ObjectExtensions
+    {
+        public static string Dump(this object o) => ObjectDumper.Dump(o);
+
+        public static string ToPrettyPrintJson(this object o) =>
+            JsonSerializer.Serialize(o, new JsonSerializerOptions { WriteIndented = true });
+    }
+}
