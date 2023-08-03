3935710a9e454c40c46f05abab988a9cd2427b34
Christopher Bocardo <397636+ccb012100@users.noreply.github.com>
2023-08-03 14:37:27 -0500

    feat: adds Exit and Now Playing buttons
    Adds buttons to Exit the application or to display the "Now Playing"
    info for media.
    
    Adds a context menu for the system tray icon that also has an Exit
    option.
    
    The Now Playing functionality is just an object Dump of the media
    properties for the time being.

--- WindowsMultiTool/MainWindow.xaml.cs
+++ WindowsMultiTool/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Drawing;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Forms;
+using System.Windows.Media;
 
 namespace WindowsMultiTool
 {
@@ -9,6 +12,9 @@ namespace WindowsMultiTool
     /// </summary>
     public partial class MainWindow : Window
     {
+        private const int BALLOON_TIP_DURATION_MS = 3000;
+        private readonly NotifyIcon _notifyIcon;
+        private readonly ContextMenuStrip _contextMenuStrip;
         public MainWindow()
         {
             Console.WriteLine("entered MainWindow");
@@ -17,20 +23,62 @@ namespace WindowsMultiTool
             // setting properties and registering event handlers
             InitializeComponent();
 
-            var notifyIcon = new System.Windows.Forms.NotifyIcon
-            {
-                BalloonTipText = "Windows Multi-tool has started",
-                Icon = new Icon(@".\Icons\multitool.ico"),
-                Text = "Windows Multi-Tool",
-                Visible = true
-            };
+            // create and initialize NotifyIcon
+            _notifyIcon = new NotifyIcon();
+            _contextMenuStrip = new ContextMenuStrip();
 
-            notifyIcon.ShowBalloonTip(3000);
+            InitializeNotifyIcon();
+            InitializeContextMenu();
+
+            _notifyIcon.ContextMenuStrip = _contextMenuStrip;
+
+            // show initial application start notification
+            _notifyIcon.ShowBalloonTip(BALLOON_TIP_DURATION_MS);
+
+            void InitializeNotifyIcon()
+            {
+                _notifyIcon.BalloonTipText = "Windows Multi-tool has started";
+                _notifyIcon.Icon = new Icon(@".\Icons\multitool.ico");
+                _notifyIcon.Text = "Windows Multi-Tool";
+                _notifyIcon.Visible = true;
+            }
+            void InitializeContextMenu()
+            {
+                var exitButton = new ToolStripButton
+                {
+                    Text = "🛑 Exit"
+                };
+                exitButton.Click += ExitButton_Click;
+
+                _contextMenuStrip.Items.Add(new ToolStripSeparator());
+                _contextMenuStrip.Items.Add(exitButton);
+
+                _contextMenuStrip.Width = 200;
+            }
         }
 
-        void ExitButton_Click(object sender, RoutedEventArgs e)
+        /// <summary>
+        /// Exit the Application on click.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void ExitButton_Click(object? sender, EventArgs? e)
         {
-            Application.Current.Shutdown();
+            System.Windows.Application.Current.Shutdown();
+        }
+
+        /// <summary>
+        /// Show Volume and Now Playing popup on click.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private async void MediaPopupButton_Click(object? sender, RoutedEventArgs? e)
+        {
+            var mediaProperties = await NowPlaying.GetMediaProperties();
+
+            _notifyIcon.BalloonTipText = mediaProperties.Dump();
+
+            _notifyIcon.ShowBalloonTip(BALLOON_TIP_DURATION_MS);
         }
     }
 }
---
+++ WindowsMultiTool/NowPlaying.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using Windows.Media.Control;
+
+namespace WindowsMultiTool
+{
+    internal static class NowPlaying
+    {
+        public static async Task<GlobalSystemMediaTransportControlsSessionMediaProperties> GetMediaProperties()
+        {
+            var session = await GetMediaSession();
+
+            return await session.TryGetMediaPropertiesAsync();
+        }
+
+        private static async Task<GlobalSystemMediaTransportControlsSession> GetMediaSession()
+        {
+            var sessionManager = await GlobalSystemMediaTransportControlsSessionManager.RequestAsync();
+
+            return sessionManager.GetCurrentSession();
+        }
+    }
+}
